94802a2e4022bfab955adb42e6d915d7ebbf00a1
ivanyu199012 <chuenyuen027546@gmail.com>
2024-04-20 19:30:17 +0900

    Init Project

---
+++ main.py
@@ -0,0 +1,20 @@
+##############################
+# Django specific settings (Please this BEFORE import model class)
+##############################
+import django
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+django.setup()
+##############################
+
+import datetime
+from db.models import Person
+
+print("Project started!!")
+person = Person()
+person.name = 'John Doe'
+person.age = 30
+person.birth_date = datetime.datetime(1963, 2, 5)
+person.save()
+print("Saved!!")
+
